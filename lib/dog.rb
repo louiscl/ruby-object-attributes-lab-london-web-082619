690c5c496cba1cf69ed919793cd6a93b230b826a
Louis Lammerting <louis.lammerting@gmail.com>
2019-08-28 09:45:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,10 @@ class Dog
     @name = given_name
   end
   
-  def name=(new_name)
-    @name = new_name
-  end
   
   def breed(kind_of_dog)
     @breed = kind_of_dog
   end
   
-  def breed=(change)
-    @breed = change
-  end
   
 end
