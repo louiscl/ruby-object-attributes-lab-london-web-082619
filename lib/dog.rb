513726133116eec64f64373b297b607e31d3199d
Louis Lammerting <louis.lammerting@gmail.com>
2019-08-28 09:46:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,24 @@
 class Dog 
   
-  def initialize(name)
-    @name
-  
-  def name(given_name)
-    @name = given_name
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
   end
   
   def name=(new_name)
     @name = new_name
   end
   
-  def breed(kind_of_dog)
-    @breed = kind_of_dog
+   def name(given_name)
+    @name = given_name
   end
   
   def breed=(change)
     @breed = change
+  end
+  
+   def breed(kind_of_dog)
+    @breed = kind_of_dog
   end
   
 end
